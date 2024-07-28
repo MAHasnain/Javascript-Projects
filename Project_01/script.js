5e802b5928713d12486b5cb854038c048ae08f0c
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-07-28 10:24:51 +0500

    Project 1 Color_switcher done

---
+++ Project_01/script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+
+console.log(buttons);
+buttons.forEach(function (button) {
+    // console.log(button);
+    button.addEventListener('click', function (e) {
+        // console.log(e);
+        // console.log(e.target);
+        if (e.target.id === "white" ) {
+            body.style.backgroundColor = e.target.id;
+        } 
+        if (e.target.id === "red") {
+            body.style.backgroundColor = e.target.id;
+        } 
+        if (e.target.id === "crimson") {
+            body.style.backgroundColor = e.target.id;
+        } 
+        if (e.target.id === "yellow") {
+            body.style.backgroundColor = e.target.id;
+        } 
+        if (e.target.id === "pink") {
+            body.style.backgroundColor = e.target.id;
+        } 
+        if (e.target.id === "brown") {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+});
